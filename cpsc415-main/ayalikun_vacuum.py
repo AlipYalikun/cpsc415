8acec6880d15e0ac5270db4d7f7e72966d409463
Alip <ayalikun@mail.umw.edu>
2023-09-09 18:38:07 -0400

    trying to make the vacuum move randomly to see what happens

--- cpsc415-main/ayalikun_vacuum.py
+++ cpsc415-main/ayalikun_vacuum.py
@@ -1,10 +1,17 @@
 from vacuum import VacuumAgent
+import random
 
 class AyalikunVacuumAgent(VacuumAgent):
+    #'Right', 'Left', 'Up', 'Down', 'Suck', 'NoOp'
     def __init__(self):
         super().__init__()
     
     def program(self, percept):
         #return super().program(percept)
-        return 'NoOp'
-    
+        isDirt,isBump = percept
+        if isDirt == "Dirty":
+            return 'Suck'
+        else:
+            directions = ['Left', 'Right', 'Up', 'Down']
+            return random.choice(directions)
+            
