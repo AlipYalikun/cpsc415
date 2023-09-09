6b93bb561986e59b9994c96b5f336ef891ae5dca
Alip <ayalikun@mail.umw.edu>
2023-09-09 17:08:57 -0400

    settingg up the ayalikun_vacuum

--- cpsc415-main/ayalikun_vacuum.py
+++ cpsc415-main/ayalikun_vacuum.py
@@ -1,1 +1,10 @@
-print("testing")
+from vacuum import VacuumAgent
+
+class ayalikunVaccumAgent(VacuumAgent):
+    def __init__(self):
+        super().__init__()
+    
+    def program(self, percept):
+        #return super().program(percept)
+        return 'NoOp'
+    
