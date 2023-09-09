163ab9f4e85377cae74dc80cf9b010fc5c6dbed2
Alip <ayalikun@mail.umw.edu>
2023-09-09 19:18:37 -0400

    trying to make vacuum go right by init it then make it go to top tight corner and start sucking row by row

--- cpsc415-main/ayalikun_vacuum.py
+++ cpsc415-main/ayalikun_vacuum.py
@@ -5,13 +5,24 @@ class AyalikunVacuumAgent(VacuumAgent):
     #'Right', 'Left', 'Up', 'Down', 'Suck', 'NoOp'
     def __init__(self):
         super().__init__()
+        self.curAct = 'Right'
     
     def program(self, percept):
         #return super().program(percept)
+        # go to up right corner and suck from row to row
         isDirt,isBump = percept
         if isDirt == "Dirty":
             return 'Suck'
         else:
-            directions = ['Left', 'Right', 'Up', 'Down']
-            return random.choice(directions)
+            if isBump == "Bump":
+                self.curAct = 'Down'
+            elif self.curAct == 'Right':
+                self.curAct = 'Up'
+            elif self.curAct == 'Up':
+                self.curAct = 'Right'
+            elif self.curAct == 'Down':
+                self.curAct = 'Left'
+            return self.curAct
+            #directions = ['Left', 'Right', 'Up', 'Down']
+            #return random.choice(directions)
             
