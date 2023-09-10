4cda400066bf0dbb3aa996ccb1a2c33495997d9b
Alip <ayalikun@mail.umw.edu>
2023-09-10 11:33:17 -0400

    trying to move it based on bumped or not

--- cpsc415-main/ayalikun_vacuum.py
+++ cpsc415-main/ayalikun_vacuum.py
@@ -14,22 +14,13 @@ class AyalikunVacuumAgent(VacuumAgent):
         if isDirt == "Dirty":
             return 'Suck'
         else:
-            if self.curAct == 'Up' and isBump=="None":
+            if isBump=="None":
                 self.curAct = 'Up'
-            elif self.curAct == 'Up' and isBump=="Bump":
-                self.curAct = 'Right'
-            elif self.curAct == 'Right' and isBump == "None":
-                self.curAct = "Up"
-            elif self.curAct == 'Right' and isBump == "Bump":
-                self.curAct = "Down"
-            elif self.curAct == 'Down' and isBump == "None":
-                self.curAct = 'Right'
-            elif self.curAct == "Down" and isBump == "Bump":
-                self.curAct = 'Left'
-            elif self.curAct == "Left" and isBump == "Bump":
-                self.curAct = 'Up'
-            elif self.curAct == "Left" and isBump == "None":
-                self.curAct = 'Down'
+            else:
+                if self.curAct == 'Up':
+                    self.curAct = 'Right'
+                elif self.curAct == 'Right':
+                    self.curAct = 'Up'
             return self.curAct
             #directions = ['Left', 'Right', 'Up', 'Down']
             #return random.choice(directions)
