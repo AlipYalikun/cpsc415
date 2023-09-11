627858691591e300e45ce60878b522ebfea67980
Alip <ayalikun@mail.umw.edu>
2023-09-11 18:39:28 -0400

    trying out a new approach where I init a list and adding dict and telling me if I have already visited this square or not

--- cpsc415-main/ayalikun_vacuum.py
+++ cpsc415-main/ayalikun_vacuum.py
@@ -5,8 +5,8 @@ class AyalikunVacuumAgent(VacuumAgent):
     #'Right', 'Left', 'Up', 'Down', 'Suck', 'NoOp'
     def __init__(self):
         super().__init__()
-        self.curAct = 'Up'
-        self.hitTopRight = True
+        #creating a list where I will be adding dictionary
+        self.listOdDic = []
     def program(self, percept):
         # go to up right corner and suck from row to row
         isDirt,isBump = percept
@@ -20,17 +20,14 @@ class AyalikunVacuumAgent(VacuumAgent):
             elif self.curAct == "Up" and isBump == "Bump":
                 self.curAct = 'Left'
             elif self.curAct == "Down" and isBump == "Bump":
-                self.curAct = 'NoOp'
+                self.curAct = 'Up'
+                #self.curAct = 'Down'
             if self.curAct == "Up" and isBump == "None":
                 self.curAct = 'Up'
             elif self.curAct == "Right" and isBump == "None":
                 self.curAct = 'Right'
-            elif self.curAct == "Up" and isBump == "None":
-                self.curAct = 'Left'
             elif self.curAct == "Down" and isBump == "None":
                 self.curAct = 'Left'
-            elif self.curAct == "NoOp":
-                self.curAct = "Left"
             return self.curAct
             #directions = ['Left', 'Right', 'Up', 'Down']
             #return random.choice(directions)
