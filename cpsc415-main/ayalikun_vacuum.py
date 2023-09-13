32bc8265df19fcdddec41a2d4b2da72eea66b42f
Alip <ayalikun@mail.umw.edu>
2023-09-13 09:07:19 -0400

    now my previous action will be stored in a dictionary I will use it to store the steps i took

--- cpsc415-main/ayalikun_vacuum.py
+++ cpsc415-main/ayalikun_vacuum.py
@@ -1,5 +1,6 @@
 from vacuum import VacuumAgent
 import random
+import uuid 
 
 class AyalikunVacuumAgent(VacuumAgent):
     #'Right', 'Left', 'Up', 'Down', 'Suck', 'NoOp'
@@ -10,25 +11,32 @@ class AyalikunVacuumAgent(VacuumAgent):
     def program(self, percept):
         # go to up right corner and suck from row to row
         isDirt,isBump = percept
+        dic = {}
         if isDirt == "Dirty":
             return 'Suck'
         else:
-            if self.curAct == "Left" and isBump == "Bump":
-                self.curAct = 'Right'
-            elif self.curAct == "Right" and isBump == "Bump":
-                self.curAct = 'Down'
-            elif self.curAct == "Up" and isBump == "Bump":
-                self.curAct = 'Left'
-            elif self.curAct == "Down" and isBump == "Bump":
-                self.curAct = 'Up'
+            directions = ['Left', 'Right', 'Up', 'Down']
+            curAct = random.choice(directions)
+            uid = str(uuid.uuid4().fields[-1])[:5]
+            self.listOdDic.append(dic.update({uid:curAct}))
+            print(dic)
+            return curAct
+            #if self.curAct == "Left" and isBump == "Bump":
+             #   self.curAct = 'Right'
+            #elif self.curAct == "Right" and isBump == "Bump":
+             #   self.curAct = 'Down'
+            #elif self.curAct == "Up" and isBump == "Bump":
+             #   self.curAct = 'Left'
+            #elif self.curAct == "Down" and isBump == "Bump":
+             #   self.curAct = 'Up'
                 #self.curAct = 'Down'
-            if self.curAct == "Up" and isBump == "None":
-                self.curAct = 'Up'
-            elif self.curAct == "Right" and isBump == "None":
-                self.curAct = 'Right'
-            elif self.curAct == "Down" and isBump == "None":
-                self.curAct = 'Left'
-            return self.curAct
-            #directions = ['Left', 'Right', 'Up', 'Down']
-            #return random.choice(directions)
-            
+            #if self.curAct == "Up" and isBump == "None":
+            #    self.curAct = 'Up'
+            #elif self.curAct == "Right" and isBump == "None":
+             #   self.curAct = 'Right'
+            #elif self.curAct == "Down" and isBump == "None":
+             #   self.curAct = 'Left'
+            #return self.curAct
+    def uid(self):
+        return str(uuid.uuid4().fields[-1])[:5]     
+ 
