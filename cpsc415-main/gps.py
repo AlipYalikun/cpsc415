6db5afd338dde359eae964f8137833613cd83767
Alip <ayalikun@mail.umw.edu>
2023-09-23 18:53:36 -0400

    the row and column way did not work saw a lecture on freecodecamp doing it that way instead

--- cpsc415-main/gps.py
+++ cpsc415-main/gps.py
@@ -18,31 +18,45 @@ def find_path(atlas, alg):
     Returns a tuple of two elements. The first is a list of city numbers,
     starting with 0 and ending with atlas.num_cities-1, that gives the
     optimal path between those two cities. The second is the total cost
-    of that path.'''
+    of that path.
+    for index, value in enumerate(row):
+                if value > num and value != math.inf:
+                    print(f'[{row_index}, {index}]: {value}')'''
     if alg == 'Dijkstras':
-        indexNumOfCity = atlas.get_num_cities() -1 
-        pathes = []
-        values = []
         a = atlas._adj_mat
         print(a)
         print(a[3,6])
         print(a[6,3])
+        # setting distance from nodes to infinite first
+        numOfCity = atlas.get_num_cities()
+        distance = [math.inf] * numOfCity
+        #root node is always zero 
+        rootCity = 0
+        distance[rootCity] = 0
+        
+        print(distance)
         #print(a[0,:])\
         num = 0
-        steps = []
+        nums = []
+        visited = atlas._nodes_expanded
         s = {}
-        step = []
         for row_index, row in enumerate(a):
-            nice = [val for val in row if val != 0 and val != math.inf]
-            if nice:
-                s[row_index] = sorted(nice)
-            for index, value in enumerate(row):
-                if value > num and value != math.inf:
-                    print(f'[{row_index}, {index}]: {value}')
-        #steps.append(s)
+            vals = [(j,num) for j, num in enumerate(row) if num > 0 and num != math.inf]
+            if vals:
+                s[row_index] = vals
+        #for node in s:
+        for node, v in s.items():
+            sorteD =  sorted(v, key=lambda x: min(x))
+            for index, value in sorteD:
+                print(f"Row {node}: Index: {index}, Value: {value}")
+                if value > num:
+                    num = value
+                    nums.append(num)
+                    num = 0
         print(s)      
         p = []
         l = 0
+        print(nums)
     return (p,l)
 
 
